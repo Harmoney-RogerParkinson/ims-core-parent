d1eae0ea48aec0d51a9ee48ac399e0a9b7a408b2
rogerparkinson <roger@senanque.co.nz>
2017-01-31 16:40:07 +1300

    setting up gitignore etc

--- ims-core-message-processor/src/main/java/com/harmoney/ims/core/messages/SpringConfig.java
+++ ims-core-message-processor/src/main/java/com/harmoney/ims/core/messages/SpringConfig.java
@@ -16,11 +16,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.test.context.ActiveProfiles;
 
 import com.salesforce.emp.connector.BayeuxParameters;
 import com.salesforce.emp.connector.EmpConnector;
