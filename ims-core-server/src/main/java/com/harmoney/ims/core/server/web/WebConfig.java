9437edb2736b2d5575691ddffb5b711e6d48c0ca
Harmoney-RogerParkinson <roger@senanque.co.nz>
2017-02-23 18:49:26 +1300

    Extended the spring configuration (untested)

--- ims-core-server/src/main/java/com/harmoney/ims/core/server/web/WebConfig.java
+++ ims-core-server/src/main/java/com/harmoney/ims/core/server/web/WebConfig.java
@@ -3,15 +3,22 @@ package com.harmoney.ims.core.server.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import com.harmoney.ims.core.messages.MessageProcessorSpringConfig;
+import com.harmoney.ims.core.queuehandler.QueueHandlerSpringConfig;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan({"com.harmoney.ims.core.server.web"})
+@Import({MessageProcessorSpringConfig.class,QueueHandlerSpringConfig.class})
+@Profile("prod")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
