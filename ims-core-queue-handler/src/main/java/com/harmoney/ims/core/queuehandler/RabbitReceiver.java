42ab003da5866741987f32833991b1dc240f59b4
Harmoney-RogerParkinson <roger@senanque.co.nz>
2017-03-15 15:28:15 +1300

    Added latching to Rabbit Receiver

--- ims-core-queue-handler/src/main/java/com/harmoney/ims/core/queuehandler/RabbitReceiver.java
+++ ims-core-queue-handler/src/main/java/com/harmoney/ims/core/queuehandler/RabbitReceiver.java
@@ -4,6 +4,7 @@
 package com.harmoney.ims.core.queuehandler;
 
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import nz.co.senanque.madura.ampq.AMPQReceiver;
 
@@ -29,16 +30,37 @@ public class RabbitReceiver {
     @Autowired private InvestorLoanTransactionProcessor investorLoanTransactionProcessor;
     @Autowired private InvestorFundTransactionProcessor investorFundTransactionProcessor;
 
-    @AMPQReceiver(queueName="ilt-queue")
+	private CountDownLatch latch;
+	private long count = 0;
+
+	@AMPQReceiver(queueName="ilt-queue")
     public void receiveILTMessage(Map<String, Map<String, Object>> message) {
         log.debug("Received <{}>", message);
         investorLoanTransactionProcessor.receiveMessage(message);
+        count();
     }
 
     @AMPQReceiver(queueName="ift-queue")
     public void receiveIFTMessage(Map<String, Map<String, Object>> message) {
         log.debug("Received <{}>", message);
         investorFundTransactionProcessor.receiveMessage(message);
+        count();
     }
 
+	public void setLatch(CountDownLatch latch) {
+		this.latch = latch;
+		for (int i = 0; i<count; i++) {
+			latch.countDown();
+		}
+	}
+	private void count() {
+		if (latch != null) {
+			latch.countDown();
+		}
+		count++;
+	}
+
+	public long getCount() {
+		return count;
+	}
 }
