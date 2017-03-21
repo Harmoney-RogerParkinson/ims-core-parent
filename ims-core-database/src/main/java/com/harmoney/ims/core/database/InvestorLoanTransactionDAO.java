4ed835c5dee9704b12c2f23cd30740ecd73a9eb5
Harmoney-RogerParkinson <roger@senanque.co.nz>
2017-03-21 18:30:58 +1300

    formatting and import tidy

--- ims-core-database/src/main/java/com/harmoney/ims/core/database/InvestorLoanTransactionDAO.java
+++ ims-core-database/src/main/java/com/harmoney/ims/core/database/InvestorLoanTransactionDAO.java
@@ -3,8 +3,6 @@
  */
 package com.harmoney.ims.core.database;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import com.harmoney.ims.core.instances.InvestorLoanTransaction;
