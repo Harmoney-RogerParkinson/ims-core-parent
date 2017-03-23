c04e3573c871760eb951064edf3fbb7a56754690
Harmoney-RogerParkinson <roger@senanque.co.nz>
2017-03-23 16:44:58 +1300

    Added new tables/objects

--- ims-core-queue-handler/src/main/java/com/harmoney/ims/core/queries/BillQuery.java
+++ ims-core-queue-handler/src/main/java/com/harmoney/ims/core/queries/BillQuery.java
@@ -6,7 +6,7 @@ package com.harmoney.ims.core.queries;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.harmoney.ims.core.queueprocessor.AmortizationScheduleProcessor;
+import com.harmoney.ims.core.queueprocessor.AccountProcessor;
 import com.sforce.soap.partner.PartnerConnection;
 import com.sforce.soap.partner.QueryResult;
 import com.sforce.soap.partner.sobject.SObject;
@@ -17,19 +17,21 @@ import com.sforce.ws.ConnectionException;
  *
  */
 @Component
-public class AmortizationScheduleQuery {
+public class BillQuery {
 	
 	@Autowired private PartnerConnection partnerConnection;
-	@Autowired private AmortizationScheduleProcessor amortizationScheduleProcessor;
+	@Autowired private AccountProcessor accountSummaryProcessor;
 	
 	public int doQuery() throws ConnectionException {
-		String queryString = "SELECT Id,Name, loan__Loan_Account__c,loan__Due_Date__c, Protect_Realised__c, "
-				+ "Sales_Commission_Realised__c, Management_Fee_Realised__c "
-				+ "FROM loan__Repayment_Schedule__c";
+		String queryString = "SELECT  Id,Name,loan__Loan_Account__c, loan__Transaction_Date__c, "
+				+ "CreatedDate, loan__Due_Date__c, loan__Due_Amt__c,loan__Payment_Satisfied__c, "
+				+ "loan__waiver_applied__c "
+				+ "FROM loan__Loan_account_Due_Details__c";
 
 		QueryResult qr = partnerConnection.query(queryString);
 		SObject[] records = qr.getRecords();
-		amortizationScheduleProcessor.processQuery(records);
+		accountSummaryProcessor.processQuery(records);
 		return records.length;
 	}
+
 }
